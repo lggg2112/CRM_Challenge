79d650d5d9eb5d8e8ab6ec50392e00f9baacfe4f
lggg2112 <lggg2112@hanmail.net>
2019-11-03 23:32:58 -0500

    Modifying Missing_email/Alphabetized

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,4 +4,12 @@ class CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
+
+  def alphabetized
+    @customers = Customer.order(:full_name)
+  end
+
+  def missing_email
+    @customers = Customer.where(email_address: '')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'customers#index'
+
+  get 'alphabetized' => 'customers#alphabetized', as: 'alphabetized'
+  get 'missing_email' => 'customers#missing_email', as: 'missing_email'
 end
