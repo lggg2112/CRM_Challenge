0e089d29f3c07d7423c9db0db1b16b967524c6c7
lggg2112 <lggg2112@hanmail.net>
2019-11-03 22:53:44 -0500

    Generate controller - customers

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,18 +1,5 @@
-ActiveAdmin.register Customer do
+# frozen_string_literal: true
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :full_name, :phone_number, :email_address, :notes
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:full_name, :phone_number, :email_address, :notes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+ActiveAdmin.register Customer do
+  permit_params :full_name, :phone_number, :email_address, :notes
 end
